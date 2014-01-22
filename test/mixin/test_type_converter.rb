8991dc24af345b915d33aceddff85539a6640d1c
y-ken <y.ken.studio@gmail.com>
2014-01-22 13:13:19 +0900

    add more unit tests

--- test/mixin/test_type_converter.rb
+++ test/mixin/test_type_converter.rb
@@ -67,27 +67,63 @@ class TypeConverterMixinTest < Test::Unit::TestCase
   def test_emit_all_types
     d1 = create_driver(%[
       tag    test.message
-      types  string:string,integer:integer,float:float,bool:bool,time:time,array:array
+      types  k_str:string,k_int:integer,k_float:float,k_bool:bool,k_time:time,k_array:array
     ], 'apache.access')
     d1.run do
       d1.emit({
-        'string'  => 'foo',
-        'integer' => '500',
-        'float'   => '0.5',
-        'bool'    => 'true',
-        'time'    => '2014-01-21 12:29:44 +0900',
-        'array'   => 'foo,1,2.0,true'
+        'k_str'   => 'foo',
+        'k_int'   => '500',
+        'k_float' => '0.5',
+        'k_bool'  => 'true',
+        'k_time'  => '2014-01-21 12:29:44 +0900',
+        'k_array' => 'foo,1,2.0,true'
       })
     end
     emits = d1.emits
     assert_equal 1, emits.length
     p emits[0]
     assert_equal 'test.message', emits[0][0] # tag
-    assert_equal 'foo', emits[0][2]['string']
-    assert_equal 500, emits[0][2]['integer']
-    assert_equal 0.5, emits[0][2]['float']
-    assert_equal true, emits[0][2]['bool']
-    assert_equal 1390274984, emits[0][2]['time']
-    assert_equal ["foo", "1", "2.0", "true"], emits[0][2]['array']
+    assert_equal 'foo', emits[0][2]['k_str']
+    assert_equal 500, emits[0][2]['k_int']
+    assert_equal 0.5, emits[0][2]['k_float']
+    assert_equal true, emits[0][2]['k_bool']
+    assert_equal 1390274984, emits[0][2]['k_time']
+    assert_equal ["foo", "1", "2.0", "true"], emits[0][2]['k_array']
+  end
+
+  def test_emit_with_other_mixin
+    d1 = create_driver(%[
+      tag                myapp.${tag}
+      remove_tag_prefix  apache.
+      types              code:integer
+    ], 'apache.access')
+    d1.run do
+      d1.emit({
+        'code' => '200'
+      })
+    end
+    emits = d1.emits
+    assert_equal 1, emits.length
+    p emits[0]
+    assert_equal 'myapp.access', emits[0][0] # tag
+    assert_equal 200, emits[0][2]['code']
+  end
+
+  def test_emit_nocast_with_other_mixin
+    d1 = create_driver(%[
+      tag                myapp.${tag}
+      remove_tag_prefix  apache.
+    ], 'apache.access')
+    d1.run do
+      d1.emit({
+        'code' => '200'
+      })
+    end
+    emits = d1.emits
+    assert_equal 1, emits.length
+    p emits[0]
+    assert_equal 'myapp.access', emits[0][0] # tag
+    assert_equal '200', emits[0][2]['code']
   end
 end
+
